0c218a6f44d63d2b9650bd444acbb9db9868d03a
Danilo <danscardoso@yahoo.com.br>
2018-10-02 17:38:44 +0000

    Melhorias e simplificações no conversor para banco relacional

--- Scripts/XML2csv.py
+++ Scripts/XML2csv.py
@@ -1,81 +1,86 @@
 # -*- coding: utf-8 -*-
 
-import glob, os, time, re, sys
-from unicodedata import normalize
+from sys import argv
 import xml.etree.ElementTree as ET #library to read xml in python, structure-comprehensible
 
-def NORM(nome): #function to remove strange characters and accents
-    nome=normalize('NFKD', nome.decode('utf-8')).encode('ASCII','ignore')
-    nome=re.sub('ã|Ã|â|Â|á|Á|à|À','A',nome)
-    nome=re.sub('ê|Ê|é|É|è|È','E',nome)
-    nome=re.sub('î|Î|í|Í|ì|Ì|ï|Ï','I',nome)
-    nome=re.sub('õ|Õ|ô|Ô|ó|Ó|ò|Ò','O',nome)
-    nome=re.sub('û|Û|ú|Ú|ù|Ù|ü|Ü','U',nome)
-    nome=re.sub('ç|Ç','C',nome)
-    nome=re.sub('ñ|Ñ','N',nome)
-    nome=re.sub("'",' ',nome)
-    nome=re.sub('"',' ',nome)
-    nome=nome.replace('*','')
-    nome=nome.replace('\\','')
-    nome=nome.replace('ª','')
-    nome=nome.replace('°','')
-    nome=nome.replace('º','')    
-    return str(nome)
-
 #recebe o nome do arquivo via CLI
-nome_arquivo = sys.argv[1];
+nome_arquivo = argv[1];
 
-print ("article_id\tato\tdata\torgao\tart_name\tart_idOficio\tart_pubName\tart_artClass\tart_artSection\tart_artSize\tart_artNotes\tart_numberPage\tart_pdfPage\tart_editionNumber\tart_texto")
+class Artigo:
+    id='null'
+    artType='null'          #ato
+    pubDate='null'          #data
+    artCategory='null'      #orgao
+    name='null'
+    idOficio='null'
+    pubName='null'
+    artClass='null'
+    artSection='null'
+    artSize='null'
+    artNotes='null'
+    numberPage='null'
+    pdfPage='null'
+    editionNumber='null'
+    Identifica='null'
+    Data='null'
+    Ementa='null'
+    Titulo='null'
+    SubTitulo='null'
+    Texto='null'
 
-tree = ET.parse(nome_arquivo)
+print ("id\tato\tdata\torgao\tname\tidOficio\tpubName\tartClass\tartSection\tartSize\tartNotes\tnumberPage\tpdfPage\teditionNumber\tIdentifica\tData\tEmenta\tTitulo\tSubtitulo\tTexto")
 
 #root is XML tag
+tree = ET.parse(nome_arquivo)
 root = tree.getroot()           #xml tag
+
+local_artigo = Artigo()
+listaAttributos = [a for a in dir(local_artigo) if not a.startswith('__') and not callable(getattr(local_artigo,a))]
 
 #para cada artigo no DOU
 for article in root:
 
-    article_id = article.attrib['id'].encode('utf-8')
-    ato = article.attrib['artType'].encode('utf-8')
-    data = article.attrib['pubDate'].encode('utf-8')
-    orgao = article.attrib['artCategory'].encode('utf-8')
-    art_name = article.attrib['name'].encode('utf-8')
-    
-    try:
-        art_idOficio = article.attrib['idOficio'].encode('utf-8')
-    except Value:
-        art_idOficio = 'null'
+    #Gerando um objeto artigo vazio
+    local_artigo = Artigo()
 
-    art_pubName = article.attrib['pubName'].encode('utf-8')
-    art_artClass = article.attrib['artClass'].encode('utf-8')
+    #Para cada atributo existente, sobrescreva o atributo local inicialmente vazio
+    for atributo in article.attrib:
+        setattr( local_artigo, atributo, article.attrib[atributo].encode('utf-8') )
 
-    try:
-        art_artSection = article.attrib['artSection'].encode('utf-8')
-    except KeyError:
-        art_artSection = 'null'
+    #Pegando cada uma das 5 tags dentro do corpo
+    for body in article:
+        for child in body:
+            try:
+                setattr( local_artigo, child.tag, child.text.encode('utf-8') )
+            except AttributeError:
+                setattr( local_artigo, child.tag, 'null' )
 
-    art_artSize = article.attrib['artSize'].encode('utf-8')
-    art_artNotes = article.attrib['artNotes'].encode('utf-8')
-    art_numberPage = article.attrib['numberPage'].encode('utf-8')
-    art_pdfPage = article.attrib['pdfPage'].encode('utf-8')
-    art_editionNumber = article.attrib['editionNumber'].encode('utf-8')
-    art_texto = article[0][5].text.encode('utf-8')
+    #iterando sobre os atributos do objeto para construir a saida para o arquivo txt
+    outputString=""
+    for item in listaAttributos:
+        outputString += getattr(local_artigo, item) + '\t'
 
-    print ( article_id + '\t' +
-            ato + '\t' +
-            data + '\t' +
-            orgao + '\t' +
-            art_name + '\t' +
-            art_idOficio + '\t' +
-            art_pubName + '\t' +
-            art_artClass + '\t' +
-            art_artSection + '\t' +
-            art_artSize + '\t' +
-            art_artNotes + '\t' +
-            art_numberPage + '\t' +
-            art_pdfPage + '\t' +
-            art_editionNumber + '\t' +
-            art_texto) #output
-    #arq_saida.write(temp+'\n')
+    #lembrando de ignorar o último tab
+    print outputString[:-1]
 
-#arq_saida.close()
+
+    #print ( local_artigo.id + '\t' +
+    #        local_artigo.artType + '\t' +
+    #        local_artigo.pubDate + '\t' +
+    #        local_artigo.artCategory + '\t' +
+    #        local_artigo.name + '\t' +
+    #        local_artigo.idOficio + '\t' +
+    #        local_artigo.pubName + '\t' +
+    #        local_artigo.artClass + '\t' +
+    #        local_artigo.artSection + '\t' +
+    #        local_artigo.artSize + '\t' +
+    #        local_artigo.artNotes + '\t' +
+    #        local_artigo.numberPage + '\t' +
+    #        local_artigo.pdfPage + '\t' +
+    #        local_artigo.editionNumber + '\t' +
+    #        local_artigo.Identifica + '\t' +
+    #        local_artigo.Data + '\t' +
+    #        local_artigo.Ementa + '\t' +
+    #        local_artigo.Titulo + '\t' +
+    #        local_artigo.SubTitulo + '\t' +
+    #        local_artigo.Texto)
